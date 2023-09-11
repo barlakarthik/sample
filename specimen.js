fa1266f2afd215d9539a6115001952b62eb13b83
barlakarthik <karthikyadav1727@gmail.com>
2023-09-11 16:16:19 +0530

    testing

---
+++ specimen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const specimen = () => {
+  return (
+    <div>specimen</div>
+  )
+}
+
+export default specimen
--- test.js
+++ test.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const test = () => {
-  return (
-    <div>test</div>
-  )
+    return (
+        <>
+            <div>test</div>
+            <div>test</div>
+            <div>test</div>
+            <div>test</div>
+        </>
+    )
 }
 
 export default test
